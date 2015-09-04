9956e62a2b50193c5354ce30141bbf1650c89139
Erik Schlegel <erik.schlegel@gmail.com>
2015-09-04 11:55:10 -0400

    Picking config details from azure

--- server.js
+++ server.js
@@ -1,17 +1,6 @@
 var http = require('http');
-var express = require('express');
-var port = 1337;
-
-process.on('uncaughtException', function(err) {
-  console.log(err);
-});
-
-var server = express();
-
-server.use(express.static(__dirname));
-
-server.listen(port, function() { 
-    console.log('listening on port ' + port);     
-    //var err = new Error('This error won't break the application...')
-    //throw err
-});
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
