c97c59a290814844ff4d9236a2f02f7e4c5e2d5d
Erik Schlegel <erik.schlegel@gmail.com>
2015-09-22 13:04:41 -0400

    Fixing continous deployment:

--- server.js
+++ server.js
@@ -12,15 +12,23 @@ server.listen(port, function() {
     //throw err
 });
 
+var timeFormatter = function () {
+    var time = new Date(), formattedDate, formattedTime, wholeThing;
+    formattedDate = daysOfWeek[time.getDay()] + ", " + monthsOfYear[time.getMonth()] + " " + time.getDate() + ", " + time.getFullYear();
+    formattedTime = time.getHours() + ":" + time.getMinutes().padLeft("0", 2) + time.getSeconds().padLeft("0", 2);
+
+    return formattedTime + ": ";
+};
+
 if(!process.env.configLoaded){
 	configUtils.load();
   var execFile = require('child_process').exec;
   var cmd = "\"./node_modules/.bin/grunt\" browserify:example";
   execFile(cmd, function(error, stdout, stderr) {
-      configUtils.save('stdout: ' + stdout, './logsOut.txt');
-      configUtils.save('stderr: ' + stderr, './logsErr.txt');
+      configUtils.save(timeFormatter() + stdout, './logsOut.txt');
+      configUtils.save(timeFormatter() + stderr, './logsErr.txt');
     if (error != null) {
-        configUtils.save('exec error: ' + error, './Err.txt');
+        configUtils.save('exec error: ' + timeFormatter() + error, './Error.txt');
     }
     process.env.configLoaded = true;
   });
